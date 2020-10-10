3f3bfea2b25e0576efdbde61c37f6d53ffdf5d0e
pratx1906 <57968015+pratx1906@users.noreply.github.com>
2020-10-11 02:14:46 +0530

    Creation

---
+++ assign06/src/assign06/LinkedListStack.java
@@ -0,0 +1,43 @@
+package assign06;
+
+import java.util.NoSuchElementException;
+
+public class LinkedListStack implements Stack {
+
+	@Override
+	public void clear() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean isEmpty() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Object peek() throws NoSuchElementException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object pop() throws NoSuchElementException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void push(Object element) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public int size() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
