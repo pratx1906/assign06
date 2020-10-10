c5d81d95d19256f97e63f078438e7ab87f046117
pratx1906 <57968015+pratx1906@users.noreply.github.com>
2020-10-11 01:37:52 +0530

    First Commit

---
+++ assign06/src/assign06/SinglyLinkedList.java
@@ -0,0 +1,80 @@
+package assign06;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class SinglyLinkedList implements List {
+
+	@Override
+	public void addFirst(Object element) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void add(int index, Object element) throws IndexOutOfBoundsException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Object getFirst() throws NoSuchElementException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object get(int index) throws IndexOutOfBoundsException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object removeFirst() throws NoSuchElementException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object remove(int index) throws IndexOutOfBoundsException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int indexOf(Object element) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int size() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public boolean isEmpty() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void clear() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Object[] toArray() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Iterator iterator() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
