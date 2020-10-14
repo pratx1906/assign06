e955da2e7de936112794a138caa3f1ee865dec1e
pratx1906 <57968015+pratx1906@users.noreply.github.com>
2020-10-15 00:38:49 +0530

    Fixed Version of RemoveFirst method

--- assign06/src/assign06/SinglyLinkedList.java
+++ assign06/src/assign06/SinglyLinkedList.java
@@ -1,223 +1,211 @@
 package assign06;
 
-import java.util.Arrays;
 import java.util.Iterator;
 import java.util.NoSuchElementException;
 
 /**
+ * This class represents a singly linked list of generic data type. implements
+ * List interface and has a nested node class for nodes creation and storing
+ * data.
  * 
  * @author Dixit && Krummenacher
  * @param <E>
  *
  */
-public class SinglyLinkedList<E> implements List<E>{
+public class SinglyLinkedList<E> implements List<E> {
 
 	/**
-	 * Private Helper class to create nodes which are used to store data in Linked List and reference to the next node
+	 * Private Helper class to create nodes which are used to store data in Linked
+	 * List and reference to the next node
 	 * 
 	 *
 	 */
-	private class Node 
-	{
+	private class Node {
 		public E data;
 		public Node next;
-		public Node(E data, Node next)
-		{
+
+		public Node(E data, Node next) {
 			this.data = data;
 			this.next = next;
 		}
 	}
-	//Instances Variables
+
+	// Instances Variables
 	private Node head;
 	private int elementCount;
-	
+
 	/**
 	 * Constructor to generate SinglyLinkList . intiallzes the intsnace variables .
 	 */
-	public SinglyLinkedList()
-	{
+	public SinglyLinkedList() {
 		this.head = null;
 		this.elementCount = 0;
 	}
-	
+
 	/**
-	 * Inserts an element at the beginning of the list.
-	 * O(1) for a singly-linked list.
+	 * Inserts an element at the beginning of the list. O(1) for a singly-linked
+	 * list.
 	 * 
 	 * @param element - the element to add
 	 */
 	@Override
-	public void addFirst(E element) 
-	{
-		head = new Node(element, head); 
+	public void addFirst(E element) {
+		head = new Node(element, head); // Assigns the first value at head
 		elementCount++;
 	}
-	
+
 	/**
-	 * Helper method to retrieve the node occurring in the list before the
-	 * node at the given position.
+	 * Helper method to retrieve the node occurring in the list before the node at
+	 * the given position.
 	 * 
-	 * NOTE: It is a precondition that pos > 0.
+	 * NOTE: It is a precondition that targetIndex > 0.
 	 * 
 	 * @param targetIndex 0-indexed position of the node
-	 * @return node at position pos-1
+	 * @return node at position targetIndex-1
 	 */
 	private Node getPrev(int targetIndex) {
-		Node temp = head;
-		for(int i = 0; i < targetIndex-1; i++)
-			temp = temp.next;
-		return temp;
+		Node indexNode = head;
+		for (int indexVal = 0; indexVal < targetIndex - 1; indexVal++)
+			indexNode = indexNode.next;
+		return indexNode;
 	}
 
-
 	/**
-	 * Inserts an element at a specific position in the list.
-	 * O(N) for a singly-linked list.
+	 * Inserts an element at a specific position in the list. O(N) for a
+	 * singly-linked list.
 	 * 
-	 * @param index - the specified position
+	 * @param index   - the specified position
 	 * @param element - the element to add
-	 * @throws IndexOutOfBoundsException if index is out of range 
-	 * (index < 0 || index > size())
+	 * @throws IndexOutOfBoundsException if index is out of range (index < 0 ||
+	 *                                   index > size())
 	 */
 	@Override
-	public void add(int index, E element) throws IndexOutOfBoundsException //TA note, best way to iterate through this.
-	{
-		if(index<0 || index>this.elementCount)
+	public void add(int index, E element) throws IndexOutOfBoundsException {
+		if (index < 0 || index > this.elementCount) // Checks for valid parameters.
 			throw new IndexOutOfBoundsException();
-		if(index==0)
-		{
-			addFirst(element);	
+		if (index == 0) // Calls addfirst() if index is 0.
+			addFirst(element);
+		else {
+			Node lastNode = getPrev(index); // Adds the reference to the new node.
+			lastNode.next = new Node(element, lastNode.next);
+			elementCount++; // Increase Size
 		}
-		else
-		{
-		
-		Node lastNode = getPrev(index);
-		lastNode.next= new Node(element,lastNode.next);
-		elementCount++;
-		}
-		
+
 	}
 
 	/**
-	 * Gets the first element in the list.
-	 * O(1) for a singly-linked list.
+	 * Gets the first element in the list. O(1) for a singly-linked list.
 	 * 
 	 * @return the first element in the list
 	 * @throws NoSuchElementException if the list is empty
 	 */
 	@Override
-	public E getFirst() throws NoSuchElementException 
-	{
-		if(!this.isEmpty())
+	public E getFirst() throws NoSuchElementException {
+		if (!this.isEmpty()) // Checks whether the ADT is empty or not
 		{
 			return this.head.data;
-		}
-		else
+		} else // Catching Exception
 		{
 			throw new NoSuchElementException();
-		}		
+		}
 	}
 
 	/**
-	 * Gets the element at a specific position in the list.
-	 * O(N) for a singly-linked list.
+	 * Gets the element at a specific position in the list. O(N) for a singly-linked
+	 * list.
 	 * 
 	 * @param index - the specified position
 	 * @return the element at the position
-	 * @throws IndexOutOfBoundsException if index is out of range 
-	 * (index < 0 || index >= size())
+	 * @throws IndexOutOfBoundsException if index is out of range (index < 0 ||
+	 *                                   index >= size())
 	 */
 	@Override
-	public E get(int index) throws IndexOutOfBoundsException 
-	{
-
-		Node res = head;
-		if(index<0 || index>this.size())
+	public E get(int index) throws IndexOutOfBoundsException {
+		Node resultNode = head; // A reference node which is used to provide result
+		if (index < 0 || index > this.size()) // Conditions for proper parameters
 			throw new IndexOutOfBoundsException();
-		
-		if(index==0)
+
+		if (index == 0) // First Index
 		{
-			res.data=getFirst();
+			resultNode.data = getFirst();
+		} else {
+			resultNode = getPrev(index); // Acquiring value of the previous node
+			resultNode = resultNode.next; // Updating it to the current value
+
 		}
-		else
-		{
-			res = getPrev(index);
-			res = res.next;
-			
-		}
-		return res.data;
-		
+		return resultNode.data; // Value
+
 	}
 
 	/**
-	 * Removes and returns the first element from the list.
-	 * O(1) for a singly-linked list.
+	 * Removes and returns the first element from the list. O(1) for a singly-linked
+	 * list.
 	 * 
 	 * @return the first element
 	 * @throws NoSuchElementException if the list is empty
 	 */
 	@Override
-	public E removeFirst() throws NoSuchElementException
-	{
-		if(this.isEmpty())
+	public E removeFirst() throws NoSuchElementException {
+		if (this.isEmpty()) // Checks for Exception
 		{
 			throw new NoSuchElementException();
+		} else {
+			Node tempRef = this.head;
+			this.head = head.next; // Update the reference
+			elementCount--; // Decreasing Size
+			return tempRef.data;
 		}
-		else
-		{
-		Node temp = this.head;
-		this.head = head.next;
-		return temp.data;
-		}
+
 	}
 
 	/**
-	 * Removes and returns the element at a specific position in the list.
-	 * O(N) for a singly-linked list.
+	 * Removes and returns the element at a specific position in the list. O(N) for
+	 * a singly-linked list.
 	 * 
 	 * @param index - the specified position
 	 * @return the element at the position
-	 * @throws IndexOutOfBoundsException if index is out of range 
-	 * (index < 0 || index >= size())
+	 * @throws IndexOutOfBoundsException if index is out of range (index < 0 ||
+	 *                                   index >= size())
 	 */
 	@Override
-	public E remove(int index) throws IndexOutOfBoundsException //TA note, best way to interate through this.
-	{
-		if(index<0 || index>this.elementCount)
+	public E remove(int index) throws IndexOutOfBoundsException {
+		Node result = head; // A reference node which is used to provide result
+		if (index < 0 || index > this.elementCount) // Condition Check
 			throw new IndexOutOfBoundsException();
-		if(index==0)
+		if (index == 0) // First Index
 			removeFirst();
-		
-		Node prev = getPrev(index);
-		Node temp = prev;
-		Node curr = temp.next;
-		prev.next = curr.next;
-		elementCount--;
-		return curr.data;
+		else {
+			Node prev = getPrev(index);
+			Node temp = prev; // Temporary node for the previous value
+			Node curr = temp.next; // Updating References
+			prev.next = curr.next;
+			elementCount--; // Decreasing Size
+			result = curr; // Making a copy to global Node for result.
+		}
+		return result.data;
+
 	}
 
 	/**
-	 * Determines the index of the first occurrence of the specified 
-	 * element in the list, or -1 if this list does not contain the element.
-	 * O(N) for a singly-linked list.
+	 * Determines the index of the first occurrence of the specified element in the
+	 * list, or -1 if this list does not contain the element. O(N) for a
+	 * singly-linked list.
 	 * 
 	 * @param element - the element to search for
 	 * @return the index of the first occurrence; -1 if the element is not found
 	 */
 	@Override
-	public int indexOf(Object element) 
-	{
-		int indexCheck =0 ;
-		Node temp = head;
-		while(temp!=null)
-		{
-			if(temp.data.equals(element))
+	public int indexOf(Object element) {
+		int indexCheck = 0; // Initial Value starts from 0
+		Node tempRef = head; // Reference Node
+		while (tempRef != null) {
+			if (tempRef.data.equals(element)) // Checks matching element type
 				return indexCheck;
 			else
 				indexCheck++;
-			temp=temp.next;
+			tempRef = tempRef.next;
 		}
-		return -1;
+		return -1; // If no such elemet is found
 	}
 
 	/**
@@ -226,8 +214,7 @@ public class SinglyLinkedList<E> implements List<E>{
 	 * @return the number of elements in this list
 	 */
 	@Override
-	public int size() 
-	{
+	public int size() {
 		return elementCount;
 	}
 
@@ -237,70 +224,59 @@ public class SinglyLinkedList<E> implements List<E>{
 	 * @return true if this collection contains no elements; false, otherwise
 	 */
 	@Override
-	public boolean isEmpty() 
-	{
+	public boolean isEmpty() {
 		return elementCount == 0;
 	}
 
 	/**
-	 * Removes all of the elements from this list.
-	 * O(1) for a singly-linked list.
+	 * Removes all of the elements from this list. O(1) for a singly-linked list.
 	 */
 	@Override
-	public void clear() 
-	{
+	public void clear() {
 		head = null;
 		elementCount = 0;
 	}
 
 	/**
-	 * Generates an array containing all of the elements in this list in proper 
-	 * sequence (from first element to last element).
-	 * O(N) for a singly-linked list.
+	 * Generates an array containing all of the elements in this list in proper
+	 * sequence (from first element to last element). O(N) for a singly-linked list.
 	 * 
 	 * @return an array containing all of the elements in this list, in order
 	 */
 	@Override
-	public Object[] toArray() //TA note, best way to interate through this.
-	{
-		Object[] result = new Object[elementCount];
-		
-		Node temp = head;
-		for(int i = 0; i < elementCount; i++)
+	public Object[] toArray() {
+		Object[] result = new Object[elementCount]; // Array Creation
+		Node tempRef = head; // Pointer Node
+		for (int i = 0; i < elementCount; i++) // Iterating through array
 		{
-			result[i] = temp.data;
-			temp = temp.next;
+			result[i] = tempRef.data; // Inputting Values
+			tempRef = tempRef.next;
 		}
-		
 		return result;
 	}
 
 	/**
-	 * @return an iterator over the elements in this list in proper sequence 
-	 * (from first element to last element)
+	 * @return an iterator over the elements in this list in proper sequence (from
+	 *         first element to last element)
 	 */
 	@Override
-	public Iterator<E> iterator() 
-	{
+	public Iterator<E> iterator() {
 		return new SinglyLinkedListIte(head);
 	}
 
-	private class SinglyLinkedListIte implements Iterator<E>
-	{
+	private class SinglyLinkedListIte implements Iterator<E> {
 		private Node curr;
 		private Node prev;
-	
-		public SinglyLinkedListIte(Node current)
-		{
+
+		public SinglyLinkedListIte(Node current) {
 			this.curr = current;
 		}
-		
+
 		/**
 		 * 
 		 */
 		@Override
-		public boolean hasNext() 
-		{			
+		public boolean hasNext() {
 			return curr != null;
 		}
 
@@ -308,59 +284,28 @@ public class SinglyLinkedList<E> implements List<E>{
 		 * 
 		 */
 		@Override
-		public E next() 
-		{
-			E temp = curr.data;		
+		public E next() {
+			E temp = curr.data;
 			prev = curr;
-			if(!hasNext())
+			if (!hasNext())
 				throw new NoSuchElementException();
-			else
-			{
+			else {
 				curr = curr.next;
 				return temp;
 			}
-			
+
 		}
-		
+
 		@Override
-		public void remove()	//TA note: Check the logic required to delete an object in memory.
-		{			
+		public void remove() {
 			prev.next = curr.next;
-			
-			if(!hasNext())
+
+			if (!hasNext())
 				throw new NoSuchElementException();
-			else
-			{
+			else {
 				curr = curr.next;
 			}
 		}
-		
-		
-		/** 
-		 * Generates and returns a textual representation of the integers in this
-		 * linked list, in order.
-		 */
-		public String toString() {
-			
-			StringBuilder result = new StringBuilder();
-			Node temp = head;
-			while(temp != null) {
-				result.append(temp.data + " ");
-				temp = temp.next;
-			}
-			return result.toString();
-		}
-
 	}
-	public static void main(String[] args) {
 
-		SinglyLinkedList<Integer> l = new SinglyLinkedList<>();
-		l.addFirst(2);
-		l.add(1, 2);
-		l.add(2, 3);
-		l.add(3, 5);
-		l.add(2, 35);
-		System.out.println(Arrays.toString(l.toArray()));
-		
-}
 }
